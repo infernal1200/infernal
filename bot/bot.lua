7358b1adb54009ddfc3c8697cda01216fb0edaa0
infernal1200 <ashidonya@gmail.com>
2015-11-06 03:29:51 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {175531746},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
